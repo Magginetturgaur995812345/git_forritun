b92d0b5d13cd3c2a8478a43a3317049110d10b01
Magginetturgaur995812345 <magnusjo294@nemi.tskoli.is>
2017-01-27 13:45:23 +0000

    Python verkefni 2

--- python.py
+++ python.py
@@ -4,3 +4,7 @@ tala2 = int(input("Sláðu inn aðra tölu "))
 tala3 = str(tala + tala2)
 tala4 = str(tala * tala2)
 print("Samanlagt er það:",tala3,"og margfaldað er það:",tala4)
+#Dæmi2
+nafn = input("Sláðu inn fornafnið þitt ")
+nafn1 = input("Sláðu inn eftirnafnið þitt ")
+print("Halló",nafn,nafn1)

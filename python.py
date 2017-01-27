5c7c0159644ae9d9850f63676485b77ca0d0bf08
Magginetturgaur995812345 <magnusjo294@nemi.tskoli.is>
2017-01-27 13:44:00 +0000

    Python verkefni

---
+++ python.py
@@ -0,0 +1,6 @@
+#Dæmi1
+tala = int(input("Sláðu inn tölu "))
+tala2 = int(input("Sláðu inn aðra tölu "))
+tala3 = str(tala + tala2)
+tala4 = str(tala * tala2)
+print("Samanlagt er það:",tala3,"og margfaldað er það:",tala4)

5d1c0c3b2482855c498094ce762207259f8e200a
Magginetturgaur995812345 <magnusjo294@nemi.tskoli.is>
2017-01-27 13:53:16 +0000

    Python verkefni 3

--- python.py
+++ python.py
@@ -8,3 +8,20 @@ print("Samanlagt er það:",tala3,"og margfaldað er það:",tala4)
 nafn = input("Sláðu inn fornafnið þitt ")
 nafn1 = input("Sláðu inn eftirnafnið þitt ")
 print("Halló",nafn,nafn1)
+#dæmi3
+upperCounter = 0
+lowerCounter = 0
+lowerAfterUpperCounter = 0
+texti = input("Sláðu inn texta: ")
+for i in texti:
+    if i == " ":
+        fyrriStafur = "o"
+    elif i.isupper():
+        upperCounter = upperCounter + 1
+        fyrriStafur = i
+    elif i.islower():
+        lowerCounter = lowerCounter + 1
+        if fyrriStafur.isupper():
+            lowerAfterUpperCounter = lowerAfterUpperCounter + 1
+        fyrriStafur = i
+print(upperCounter,"hástafir, og",lowerCounter,"lágstafir. ",lowerAfterUpperCounter,"lágstafir komu strax eftir hástaf")
